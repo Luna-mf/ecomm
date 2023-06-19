7671b45e0d04196e7cc07ab12dcd0eff5213fba4
Luna <luna.matana@pagonxt.com>
2023-06-19 14:32:59 -0300

    Updates

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -3,22 +3,6 @@ import CategoryService from "./CategoryService.js";
 const args = process.argv;
 
 async function processarComando(argumentos) {
-    // utilizar switch / case
-    /* Exemplo:
-            const expr = 'Papayas';
-        switch (expr) {
-        case 'Oranges':
-            console.log('Oranges are $0.59 a pound.');
-            break;
-        case 'Mangoes':
-        case 'Papayas':
-            console.log('Mangoes and papayas are $2.79 a pound.');
-            // Expected output: "Mangoes and papayas are $2.79 a pound."
-            break;
-        default:
-            console.log(`Sorry, we are out of ${expr}.`);
-        }
-    */
     switch (argumentos[2]) {
     case "--listarCategorias":
         await CategoryService.findCategories();
