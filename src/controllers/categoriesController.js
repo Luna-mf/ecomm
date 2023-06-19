98773c02a39ff2f491dcb6598aca8e80e426a07a
Luna Matana Fortes <luna.matana@pagonxt.com>
2023-06-19 16:03:18 -0300

    Added list category by id route;

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -24,72 +24,13 @@ class CategoryController {
 
         await livro.save();
         res.status(201).send(livro.toJSON());
-        /* livro.save((err) => {
-            if (err) {
-                res.status(500).send({ message: `${err.message} - falha ao cadastrar livro.` });
-            } else {
-                res.status(201).send(livro.toJSON());
-            }
-        }); */
     };
 
-    /* static listarLivroPorId = (req, res) => {
-        const { id } = req.params;
-
-        livros.findById(id)
-            .populate("autor", "nome")
-            .exec((err, livros) => {
-                if (err) {
-                    res.status(400).send({ message: `${err.message} -
-                     Id do livro não localizado.` });
-                } else {
-                    res.status(200).send(livros);
-                }
-            });
+    static listarPorId = async (req, res) => {
+        const id = req.params.id;
+        const categorieFinded = await categories.find({ _id: id });
+        res.status(200).json(categorieFinded);
     };
-
-    static cadastrarLivro = (req, res) => {
-        const livro = new livros(req.body);
-
-        livro.save((err) => {
-            if (err) {
-                res.status(500).send({ message: `${err.message} - falha ao cadastrar livro.` });
-            } else {
-                res.status(201).send(livro.toJSON());
-            }
-        });
-    };
-
-    static atualizarLivro = (req, res) => {
-        const { id } = req.params;
-
-        livros.findByIdAndUpdate(id, { $set: req.body }, (err) => {
-            if (!err) {
-                res.status(200).send({ message: "Livro atualizado com sucesso" });
-            } else {
-                res.status(500).send({ message: err.message });
-            }
-        });
-    };
-
-    static excluirLivro = (req, res) => {
-        const { id } = req.params;
-
-        livros.findByIdAndDelete(id, (err) => {
-            if (!err) {
-                res.status(200).send({ message: "Livro removido com sucesso" });
-            } else {
-                res.status(500).send({ message: err.message });
-            }
-        });
-    };
-
-    static listarLivroPorEditora = (req, res) => {
-        const { editora } = req.query;
-
-        livros.find({ editora }, {}, (err, livros) => {
-            res.status(200).send(livros);
-        });
-    }; */
 }
+
 export default CategoryController;
--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
-import LivroController from "../controllers/categoriesController.js";
+import CategoryController from "../controllers/categoriesController.js";
 
 const base = "/api";
 
 const router = express.Router();
 
 router
-    .get(`${base}/categories`, LivroController.listarCategorias)
-    .post(`${base}/categories`, LivroController.cadastrarCategoria);
+    .get(`${base}/categories`, CategoryController.listarCategorias)
+    .get(`${base}/categories/:id`, CategoryController.listarPorId)
+    .post(`${base}/categories`, CategoryController.cadastrarCategoria);
 
 export default router;
