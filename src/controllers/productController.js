ad9b8eb1f20acb0b3f48eea6946aa44e1a99bb30
Luna Matana Fortes <luna.matana@pagonxt.com>
2023-06-20 13:03:04 -0300

    Added validations with regex for create products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -7,29 +7,29 @@ async function IsValidCategoryId(idCategory) {
     return !(categoryFunded == null || categoryFunded.length === 0);
 }
 
-async function ValidateProduct(body) {
-    if (body.nome == null || body.nome.length < 3) {
-        throw new Error("Invalid argument: nome");
-    }
-    if (body.precoUnitario <= 0) {
-        throw new Error("Invalid argument: preço unitário deve ser maior que zero");
-    }
-    if (body.quantidadeEmEstoque <= 0 || body.quantidadeEmEstoque >= 10000) {
-        throw new Error("Invalid argument: quanitdade em estoque deve estar entre 1 e 9999");
-    }
+function validateWithRegex(expression, text) {
+    const capturas = [...text.matchAll(expression)];
+    return (capturas != null && capturas.length > 0);
+}
 
+async function ValidateProduct(body) {
     const id = new mongoose.Types.ObjectId(body.categoria.id);
     const isValidCategory = await IsValidCategoryId(id);
 
     if (!isValidCategory) {
         throw new Error("Invalid argument: Id da categoria não encontrado");
+    } else if (body.nome == null || body.nome.length < 3) {
+        throw new Error("Invalid argument: nome");
+    } else if (body.precoUnitario <= 0) {
+        throw new Error("Invalid argument: preço unitário deve ser maior que zero");
+    } else if (body.quantidadeEmEstoque <= 0 || body.quantidadeEmEstoque >= 10000) {
+        throw new Error("Invalid argument: quanitdade em estoque deve estar entre 1 e 9999");
+    } else if (!validateWithRegex(/^[^0-9]/gm, body.nome)) {
+        throw new Error("Invalid argument: nome inválido, o nome não pode começar com um número!");
+    } else if (!validateWithRegex(/^[A-Za-z0-9- ]+$/gm, body.slug)) {
+        throw new Error("Invalid argument: slug só pode conter letras maiúsculas e minúsculas,"
+        + "números e hífens");
     }
-
-    /*
-    Validações que ainda precisam ser feitas:
-        - Nome do produto
-        - slug
-    */
 }
 
 class ProductController {
@@ -54,20 +54,34 @@ class ProductController {
 
     static listById = async (req, res) => {
         const id = req.params.id;
-        const productFinded = await products.find({ _id: id });
-        res.status(200).json(productFinded).end();
+        try {
+            const productFinded = await products.find({ _id: id });
+            res.status(200).json(productFinded).end();
+        } catch (error) {
+            res.status(400).send({ errorMessage: error.message }).end();
+        }
     };
 
     static updateById = async (req, res) => {
         const id = req.params.id;
-        await products.updateOne({ _id: id }, body);
-        res.status(204).end();
+
+        try {
+            await products.updateOne({ _id: id }, req.body);
+            res.status(204).end();
+        } catch (error) {
+            res.status(400).send({ errorMessage: error.message }).end();
+        }
     };
 
     static deleteById = async (req, res) => {
         const id = req.params.id;
-        await products.deleteOne({ _id: id });
-        res.status(204).end();
+
+        try {
+            await products.deleteOne({ _id: id });
+            res.status(204).end();
+        } catch (error) {
+            res.status(400).send({ errorMessage: error.message }).end();
+        }
     };
 }
 
